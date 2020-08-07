004d91de565a19aa8fa8d30a83f894fb6efb1df6
MdTaufiqurRahman <taufiqur.anik.bd@gmail.com>
2020-08-07 20:39:35 +0600

    working

---
+++ true-false.js
@@ -0,0 +1,20 @@
+//Falsy :
+//if the given value is: false
+//if value is: 0.
+//if value is empty string : ""
+//is value is: undefined
+//if value is :Null
+//if value is :NaN (Not a Number)
+
+//Truthy :
+//Not a empty string: '0' , 'Anik', '<space>'
+//make a object: {}
+//make a empty array: []
+
+let name={};
+if(name){
+    console.log("Condition is True");
+}
+else{
+    console.log("Condition is True");
+}
