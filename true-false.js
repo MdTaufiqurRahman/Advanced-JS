4bd62960dfc7d581ec380b301b546cc481d2ea60
MdTaufiqurRahman <taufiqur.anik.bd@gmail.com>
2020-08-07 20:48:22 +0600

    true vs false

--- true-false.js
+++ true-false.js
@@ -1,17 +1,25 @@
 //Falsy :
-//if the given value is: false
-//if value is: 0.
-//if value is empty string : ""
-//is value is: undefined
-//if value is :Null
-//if value is :NaN (Not a Number)
+//1. if the given value is: false
+//2. if value is: 0.
+//3. if value is empty string : ""
+//4. if value is: undefined
+//5. if value is :Null
+//6. if value is :NaN (Not a Number)
 
 //Truthy :
-//Not a empty string: '0' , 'Anik', '<space>'
-//make a object: {}
-//make a empty array: []
+//1. Not a empty string: '0' , 'Anik', '<space>'
+//2. make a object: {}
+//3. make a empty array: []
 
-let name={};
+let name=[]; //this is true
+if(name){
+    console.log("Condition is True");
+}
+else{
+    console.log("Condition is True");
+}
+
+let name=""; //this is false
 if(name){
     console.log("Condition is True");
 }
