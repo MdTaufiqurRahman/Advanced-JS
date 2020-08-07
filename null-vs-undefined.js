05a51395a17a24cdbb83bcb67adb2a24ec978195
MdTaufiqurRahman <taufiqur.anik.bd@gmail.com>
2020-08-07 22:13:51 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,27 @@
+let pakhi;
+
+
+function add(num1,num2){
+    console.log(num1+num2); //do not return result so that it's undefined. 
+}
+     const result=add(13,82)
+     console.log(result)
+
+
+function add(num1,num2){
+    console.log(num1,num2);  //do not return result so that it's undefined. 
+// }
+const result=add(13); //if i do not pass perameter then it undefined.
+     console.log(result);
+
+
+const premik={name:"anik", phone:555868}
+console.log(premik.gf); //In premik object gf property absence so its undefined.
+
+
+let fun=undefined; //a reserved word always gives undefined
+console.log(fun);
+//i can get undefined in many ways.
+
+let ages=[2,5,6]
+console.log(ages[15]);
